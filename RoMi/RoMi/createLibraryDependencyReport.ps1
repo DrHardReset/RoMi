80581016d141945578d2d18368493068abd349f1
DrHardReset <brillebralle@web.de>
2024-01-11 22:40:36 +0100

    Changed: creation of library dependency report

---
+++ RoMi/RoMi/createLibraryDependencyReport.ps1
@@ -0,0 +1,16 @@
+$solutionDir=$args[0]
+$reportPath=$args[1] + "Assets\LibraryDependencyReport.json"
+
+Write-Host "LibraryDependencyReport for solution" $solutionDir "will be generated to" $reportPath"."
+$processOptions = @{
+    FilePath = "dotnet"
+    RedirectStandardOutput = $reportPath
+}
+
+Start-Process @processOptions -Wait "list $solutionDir package --format json"
+
+$pathPattern = $solutionDir.Replace("\","/")
+$content = [System.IO.File]::ReadAllText($reportPath).Replace($pathPattern,"<solutionDir>/")
+$result = [System.IO.File]::WriteAllText($reportPath, $content)
+
+exit $LASTEXITCODE
